9e0b185f40303959f155044cb8213550d76b9451
dhanapathim <dhanapathi.marepalli@accionlabs.com>
2025-10-06 13:12:44 +0530

    QUM Action and Validation.

---
+++ Learning/Playwright-all/Cision/SMM/tests/create-post.spec.js
@@ -0,0 +1,83 @@
+// tests/brandwatch-login.spec.js
+import { test, expect, chromium } from '@playwright/test';
+import { qumAction } from '../utils/qumAction.js';
+import { qumValidation } from '../utils/qumValidation.js';
+
+test.describe('Instagram', () => {
+  test.beforeAll(async() => {
+  });
+
+  test.afterAll(async() => {
+
+  });
+
+  test('Login', async ({page, context, baseURL }) => {
+  
+    // 1. Open the url from config
+    await qumAction('Open Url', page, async () => {
+      await page.goto(baseURL, { waitUntil: 'load' });
+    });
+
+    // Assert title
+    await qumValidation('Validate url post page load.', page, async () => {
+      await expect(page).toHaveTitle('Brandwatch');
+    });
+
+    // await page.goto(baseURL, { waitUntil: 'load' });
+
+    
+    
+    // 2. Accept Cookies
+    await qumAction('Accept Cookies', page, async () => {
+      await page.locator("//*[@id='onetrust-accept-btn-handler']").click();
+    });
+
+    // 3. Click Sign in button
+    await qumAction('Click Sign In Tab', page, async () => {
+      await page.locator('//*[@id="spanrotate"]').click();
+    });
+    await page.screenshot({ path: './screenshots/signin-clicked.png' });
+
+    // 4. Click "Social Media Management"
+    await qumAction('Select Social Media Management', page, async () => {
+      const ssmLocator = page.locator("(//span[contains(@class,'f3-lg-xl u-hover__underline-target span-head')][contains(text(),'Social Media')])[1]");
+      await ssmLocator.click();
+    });
+
+    const [newTab] = await Promise.all([
+      context.waitForEvent('page'),
+    ]);
+
+    console.log(await newTab.title());
+    
+    // 5. Enter username.
+    await qumAction('Enter username', newTab, async () => {
+      await newTab.locator('//*[@id="lookupUsername"]').fill(process.env.BW_USERNAME);
+    });
+
+    // 6. Click Next to go to Password Page
+    await qumAction('Click Next go to password Page', newTab, async () => {
+      await newTab.locator('//*[@id="continue-btn-text"]').click();
+    });
+
+    await newTab.waitForLoadState('load');
+
+    // 7. Enter Password
+    await qumAction('Enter Password', newTab, async () => {
+      await newTab.locator('//*[@id="password"]').fill(process.env.BW_PASSWORD);
+    });
+
+    // 8. Click Sign in
+    await qumAction('Click SignIn Button', newTab, async () => {
+      await newTab.locator('//*[@id="signin-btn-text"]').click();
+    });
+    await newTab.waitForLoadState('load');
+
+    // Final validation
+    // await expect(newTab).toHaveTitle('Social Media Management');
+    await qumValidation('Validate url post page load.', page, async () => {
+      await expect(newTab).toHaveTitle('Social Media Management')
+    });
+  });
+
+});
---
+++ Learning/Playwright-all/Cision/SMM/utils/qumValidation.js
@@ -0,0 +1,57 @@
+import { test } from '@playwright/test';
+
+/**
+ * namedStep - Wraps a Playwright test step with:
+ * - Step description + test info
+ * - Network + navigation timings (name, type, duration, status)
+ *
+ * @param {string} description - Step description
+ * @param {Page} page - Playwright Page object
+ * @param {Function} fn - Async step actions
+ */
+export async function qumValidation(description, page, fn) {
+  const info = test.info();
+
+  if (!page || typeof page.evaluate !== 'function') {
+    throw new Error(`namedStep expected a Playwright Page, but got: ${page}`);
+  }
+
+  console.log(`\n--- Start of Action ---`);
+  const [task, scenario, step] = info.titlePath;
+  const taskName = formatTaskName(task);
+
+  console.log(`Task: ${taskName}`);
+  console.log(`Scenario: ${scenario}`);
+  console.log(`Step: ${step}`);
+  console.log(`Action: ${description}`);
+
+  // Execute the step
+  await test.step(description, async () => {
+    let valid = false
+    try {
+      await fn();
+      valid = true;
+    } catch(e) {
+      console.error(`Validation failed for Action ${description}.`);
+      throw Error(`Validation failed for Action ${description}.Error details are `, e);
+    }
+    console.log(`Validation is ${valid}`);
+  });
+  console.log(`--- END of Action ${description} ---\n`);
+}
+
+/**
+ * Formats the task/spec filename:
+ * - Removes .spec.ts / .spec.js / .ts / .js
+ * - Replaces - and _ with spaces
+ * - Capitalizes each word
+ */
+function formatTaskName(fileName) {
+  return fileName
+    .replace(/\.(spec|test)?\.(ts|js)$/i, '')
+    .replace(/[-_]+/g, ' ')
+    .split(' ')
+    .filter(Boolean)
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+}
